8db9c17e8c595cd53d6fffa4cad6598922eb3e77
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-05-22 22:57:16 +0100

    chat

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const port = 3000;
 const mongoose = require("mongoose");
 const crud = require("./controllers/crud")
 
@@ -17,23 +16,58 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// db
-mongoose.connect("mongodb://localhost:27017/",
-  console.log("db connected"));
+// DB
+mongoose.connect("mongodb://localhost:27017/", console.log("db connected"));
 
-app.listen(port,
-  console.log(`Server is active on port ${port}`));
+// Socket.io
+var server = require('http').createServer(app);
+var io = require("socket.io")(server);
+var Chat = require("./models/schema");
+
+io.on('connection', socket => {
+  socket.broadcast.emit('user connected');
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user disconnected');
+  });
+
+  socket.on("chat message", async data => {
+    const message = new Chat({ pseudo: data.pseudo, message: data.message });
+    socket.broadcast.emit("chat message", { data: message });
+    await message.save();
+  });
+
+  socket.on('typing', data => {
+    socket.broadcast.emit('typing', { author: data.author });
+  });
+});
+
+// Routes
+app.get('/chat', async function(req, res, next) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/messages', async function(req, res, next) {
+  try {
+    const messages = await Chat.find({});
+    res.json(messages);
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.use(userRoutes);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/api",crud)
+app.use("/api", crud);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -50,7 +84,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const port = 3000;
+server.listen(port, console.log(`Server is active on port ${port}`));
 
 module.exports = app;
